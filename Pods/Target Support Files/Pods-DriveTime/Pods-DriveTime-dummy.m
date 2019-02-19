8ebd527df1dbdc7cee175042a0da7e4ddfcce50b
wingsuncheung <brendon.cheung@outlook.com>
2019-02-19 15:30:01 -0500

    first commit

---
+++ Pods/Target Support Files/Pods-DriveTime/Pods-DriveTime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DriveTime : NSObject
+@end
+@implementation PodsDummy_Pods_DriveTime
+@end
---
+++ Pods/Target Support Files/Pods-DriveTimeUITests/Pods-DriveTimeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DriveTimeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DriveTimeUITests
+@end
